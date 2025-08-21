a2eb0caadd9b6fff3a4c28dc7dc71cdef42d6f5a
Darshana Thakare <124777481+darshanathakare29@users.noreply.github.com>
2025-08-22 02:33:35 +0530

    Add files via upload

---
+++ QuizApp_Java/src/maccess/Rules.java
@@ -0,0 +1,76 @@
+
+package maccess;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Rules extends JFrame implements ActionListener{
+
+    String name;
+    JButton start, back;
+    
+    Rules(String name) {
+        this.name = name;
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("Welcome " + name + " to Simple Minds");
+        heading.setBounds(50, 20, 700, 30);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 28));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+        
+        JLabel rules = new JLabel();
+        rules.setBounds(20, 90, 700, 350);
+        rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        rules.setText(
+            "<html>"+ 
+                "1.Each team should have 3-4 participants. All team members must be registered prior to the event." + "<br><br>" +
+                "2. Each team has 15 seconds to answer a question once it is read or displayed" + "<br><br>" +
+                "3. You may have lot of options in life but here all the questions are compulsory" + "<br><br>" +
+                "4. Correct answers earn 10 points." + "<br><br>" +
+                "5. The quizmaster's decision is final in all matters related to the quiz" + "<br><br>" +
+                "6. Do not get nervous if your friend is answering more questions, may be he/she is doing Jai Mata Di" + "<br><br>" +
+                "7. Brace yourself, this paper is not for the faint hearted" + "<br><br>" +
+                "8. May you know more than what John Snow knows, Good Luck" + "<br><br>" +
+            "<html>"
+        );
+        add(rules);
+        
+        back = new JButton("Back");
+        back.setBounds(250, 500, 100, 30);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+        
+        start = new JButton("Start");
+        start.setBounds(400, 500, 100, 30);
+        start.setBackground(new Color(30, 144, 254));
+        start.setForeground(Color.WHITE);
+        start.addActionListener(this);
+        add(start);
+        
+        setSize(800, 650);
+        setLocation(350, 100);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == start) {
+            setVisible(false);
+            new Quiz(name);
+        } else {
+            setVisible(false);
+            new Login();
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Rules("User");
+    }
+}
+
+
+ 
